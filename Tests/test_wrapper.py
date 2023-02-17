a6506c90681281b8b045658cd8c8052083266c84
Aaron <aaron.bowman@leargasiere.com>
2023-02-17 00:31:40 +0000

    -Updated tests with new coverage for wrapper -Updated to v 0.0.2
    Signed-off-by: Aaron <aaron.bowman@leargasiere.com>

--- Tests/test_wrapper.py
+++ Tests/test_wrapper.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+import vcr
+
 from OireachtasAPI import wrapper
 
 
@@ -36,3 +38,34 @@ class TestWrapper(TestCase):
     def test__fetch_endpoint_members(self):
         test_case = wrapper.Wrapper()._fetch_endpoint(endpoint_name='members')
         self.assertEqual(test_case, 'https://api.oireachtas.ie/v1/members')
+
+    #Testing that the wrapper actually returns the same info as the API make request fuction
+    @vcr.use_cassette('fixtures/cassettes/legislation.yaml', record_mode='new_episodes')
+    def test_wrapper_make_legislation_request(self):
+        test_case = wrapper.Wrapper().wrapper_make_request(endpoint_name='legislation', params={'limit':1})
+        test_case = test_case.json()
+        self.assertIn('billCount', test_case['head']['counts'])
+
+    @vcr.use_cassette('fixtures/cassettes/questions.yaml', record_mode='new_episodes')
+    def test_wrapper_make_question_request(self):
+        test_case = wrapper.Wrapper().wrapper_make_request(endpoint_name='questions', params={'limit':1})
+        test_case = test_case.json()
+        self.assertIn('questionCount', test_case['head']['counts'])
+
+    @vcr.use_cassette('fixtures/cassettes/debates.yaml', record_mode='new_episodes')
+    def test_wrapper_make_debate_request(self):
+        test_case = wrapper.Wrapper().wrapper_make_request(endpoint_name='debates', params={'limit':1})
+        test_case = test_case.json()
+        self.assertIn('debateCount', test_case['head']['counts'])
+
+    @vcr.use_cassette('fixtures/cassettes/constituencies.yaml', record_mode='new_episodes')
+    def test_wrapper_make_constituencies_request(self):
+        test_case = wrapper.Wrapper().wrapper_make_request(endpoint_name='constituencies', params={'limit':1})
+        test_case = test_case.json()
+        self.assertIn('constituencyCount', test_case['head']['counts'])
+
+    @vcr.use_cassette('fixtures/cassettes/parties.yaml', record_mode='new_episodes')
+    def test_wrapper_make_parties_request(self):
+        test_case = wrapper.Wrapper().wrapper_make_request(endpoint_name='parties', params={'limit':1})
+        test_case = test_case.json()
+        self.assertIn('partyCount', test_case['head']['counts'])
